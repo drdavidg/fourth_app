ff7505beba8b0a772119e4c6b8fd1f43bbc74c2c
David Gonen <david@curiousminds.com>
2011-12-04 13:13:55 -0800

    first fourth app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FourthApp::Application.config.secret_token = 'aeba525d61e8c4c5b81a38fb660cccc197f6d1334d2511b12f253c419bf1c725e42a57bee2891048f867b54c5f63c474bf74e5d59cb78e3c04b5d59d3c1d9823'
