e84849e2df297daa2c2ced0aa2f9a44f4fe174c9
David Gonen <david@curiousminds.com>
2011-12-07 17:49:26 -0800

    Done with static pages.  Didn't use Rspec used the one built into Rails thats in the /test directory

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -10,5 +10,24 @@ class PagesControllerTest < ActionController::TestCase
     get :contact
     assert_response :success
   end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+  
+  test "Home should have the right title" do
+    get :home
+    assert_select 'title', "Ruby on Rails Tutorial Sample App | Home"
+  end
 
+  test "About should have the right title" do
+    get :about
+    assert_select 'title', "Ruby on Rails Tutorial Sample App | About"
+  end
+
+  test "Contact should have the right title" do
+    get :contact
+    assert_select 'title', "Ruby on Rails Tutorial Sample App | Contact"
+  end
 end
